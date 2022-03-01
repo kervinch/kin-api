edf3172fee09ec6c7d0b5b564a0adfb3f273ec16
Kervin Christianata <kervin@ternakuang.id>
2022-03-01 21:09:43 +0700

    forget password

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -29,7 +29,11 @@ func (app *application) routes() http.Handler {
 	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
+
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "v1/tokens/activation", app.createActivationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 
 	// metrics
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
