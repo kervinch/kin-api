78f147b4f948a678a52eeb66c8f7192d3da61d3a
Kervin Christianata <kervin@ternakuang.id>
2022-06-09 12:42:30 +0700

    xendit callback

--- cmd/api/callbacks.go
+++ cmd/api/callbacks.go
@@ -82,3 +82,42 @@ func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Re
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) testInvoiceCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	headers := make(http.Header)
+	headers.Set("x-callback-token", os.Getenv("XENDIT_CALLBACK_VERIFICATION_TOKEN"))
+
+	var callbackPayload struct {
+		ID                     string  `json:"id"`
+		ExternalID             string  `json:"external_id"`
+		UserID                 string  `json:"user_id"`
+		IsHigh                 bool    `json:"is_high"`
+		PaymentMethod          string  `json:"payment_method"`
+		Status                 string  `json:"status"`
+		MerchantName           string  `json:"merchant_name"`
+		Amount                 float64 `json:"amount"`
+		PaidAmount             float64 `json:"paid_amount"`
+		BankCode               string  `json:"bank_code"`
+		PaidAt                 string  `json:"paid_at"`
+		PayerEmail             string  `json:"payer_email"`
+		Description            string  `json:"description"`
+		AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
+		FeesPaidAmount         float64 `json:"fees_paid_amount"`
+		Updated                string  `json:"updated"`
+		Created                string  `json:"created"`
+		Currency               string  `json:"currency"`
+		PaymentChannel         string  `json:"payment_channel"`
+		PaymentDestination     string  `json:"payment_destination"`
+	}
+
+	err := app.readJSON(w, r, &callbackPayload)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), nil, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
