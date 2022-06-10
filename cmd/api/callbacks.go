1143dd077da6535de09c2e9d81c9a23dfd5c47bd
Kervin Christianata <kervin@ternakuang.id>
2022-06-10 14:41:02 +0700

    xendit callback

--- cmd/api/callbacks.go
+++ cmd/api/callbacks.go
@@ -18,26 +18,26 @@ func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	var callbackPayload struct {
-		ID                     string  `json:"id"`
-		ExternalID             string  `json:"external_id"`
-		UserID                 string  `json:"user_id"`
-		IsHigh                 bool    `json:"is_high"`
-		PaymentMethod          string  `json:"payment_method"`
-		Status                 string  `json:"status"`
-		MerchantName           string  `json:"merchant_name"`
-		Amount                 float64 `json:"amount"`
-		PaidAmount             float64 `json:"paid_amount"`
-		BankCode               string  `json:"bank_code"`
-		PaidAt                 string  `json:"paid_at"`
-		PayerEmail             string  `json:"payer_email"`
-		Description            string  `json:"description"`
-		AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
-		FeesPaidAmount         float64 `json:"fees_paid_amount"`
-		Updated                string  `json:"updated"`
-		Created                string  `json:"created"`
-		Currency               string  `json:"currency"`
-		PaymentChannel         string  `json:"payment_channel"`
-		PaymentDestination     string  `json:"payment_destination"`
+		// ID                     string  `json:"id"`
+		ExternalID string `json:"external_id"`
+		// UserID                 string  `json:"user_id"`
+		// IsHigh                 bool    `json:"is_high"`
+		// PaymentMethod          string  `json:"payment_method"`
+		// Status                 string  `json:"status"`
+		// MerchantName           string  `json:"merchant_name"`
+		// Amount                 float64 `json:"amount"`
+		// PaidAmount             float64 `json:"paid_amount"`
+		// BankCode               string  `json:"bank_code"`
+		// PaidAt                 string  `json:"paid_at"`
+		// PayerEmail             string  `json:"payer_email"`
+		// Description            string  `json:"description"`
+		// AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
+		// FeesPaidAmount         float64 `json:"fees_paid_amount"`
+		// Updated                string  `json:"updated"`
+		// Created                string  `json:"created"`
+		// Currency               string  `json:"currency"`
+		// PaymentChannel         string  `json:"payment_channel"`
+		// PaymentDestination     string  `json:"payment_destination"`
 	}
 
 	err := app.readJSON(w, r, &callbackPayload)
