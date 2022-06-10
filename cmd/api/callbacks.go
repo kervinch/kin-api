f7e00062b2cb46872e459f20c3102f44d63c797e
Kervin Christianata <kervin@ternakuang.id>
2022-06-10 15:16:24 +0700

    xendit callback

--- cmd/api/callbacks.go
+++ cmd/api/callbacks.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"os"
+
+	"github.com/xendit/xendit-go"
 )
 
 // ====================================================================================
@@ -18,26 +20,39 @@ func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	var callbackPayload struct {
-		ID                     string  `json:"id,omitempty"`
-		ExternalID             string  `json:"external_id,omitempty"`
-		UserID                 string  `json:"user_id,omitempty"`
-		IsHigh                 bool    `json:"is_high,omitempty"`
-		PaymentMethod          string  `json:"payment_method,omitempty"`
-		Status                 string  `json:"status,omitempty"`
-		MerchantName           string  `json:"merchant_name,omitempty"`
-		Amount                 float64 `json:"amount,omitempty"`
-		PaidAmount             float64 `json:"paid_amount,omitempty"`
-		BankCode               string  `json:"bank_code,omitempty"`
-		PaidAt                 string  `json:"paid_at,omitempty"`
-		PayerEmail             string  `json:"payer_email,omitempty"`
-		Description            string  `json:"description,omitempty"`
-		AdjustedReceivedAmount float64 `json:"adjusted_received_amount,omitempty"`
-		FeesPaidAmount         float64 `json:"fees_paid_amount,omitempty"`
-		Updated                string  `json:"updated,omitempty"`
-		Created                string  `json:"created,omitempty"`
-		Currency               string  `json:"currency,omitempty"`
-		PaymentChannel         string  `json:"payment_channel,omitempty"`
-		PaymentDestination     string  `json:"payment_destination,omitempty"`
+		ID                           string                      `json:"id,omitempty"`
+		ExternalID                   string                      `json:"external_id,omitempty"`
+		UserID                       string                      `json:"user_id,omitempty"`
+		IsHigh                       bool                        `json:"is_high,omitempty"`
+		PaymentMethod                string                      `json:"payment_method,omitempty"`
+		Status                       string                      `json:"status,omitempty"`
+		MerchantName                 string                      `json:"merchant_name,omitempty"`
+		Amount                       int                         `json:"amount,omitempty"`
+		PaidAmount                   float64                     `json:"paid_amount,omitempty"`
+		BankCode                     int                         `json:"bank_code,omitempty"`
+		PaidAt                       string                      `json:"paid_at,omitempty"`
+		PayerEmail                   string                      `json:"payer_email,omitempty"`
+		Description                  string                      `json:"description,omitempty"`
+		AdjustedReceivedAmount       float64                     `json:"adjusted_received_amount,omitempty"`
+		FeesPaidAmount               float64                     `json:"fees_paid_amount,omitempty"`
+		Updated                      string                      `json:"updated,omitempty"`
+		Created                      string                      `json:"created,omitempty"`
+		Currency                     string                      `json:"currency,omitempty"`
+		PaymentChannel               string                      `json:"payment_channel,omitempty"`
+		PaymentDestination           string                      `json:"payment_destination,omitempty"`
+		MerchantProfileURL           string                      `json:"merchant_profile_url,omitempty"`
+		PaymentDetail                xendit.InvoicePaymentDetail `json:"payment_detail,omitempty"`
+		SuccessRedirectURL           string                      `json:"success_redirect_url,omitempty"`
+		FailureRedirectURL           string                      `json:"failure_redirect_url,omitempty"`
+		MidLabel                     string                      `json:"mid_label,omitempty"`
+		CreditCardChargeID           string                      `json:"credit_card_charge_id,omitempty"`
+		Item                         []xendit.InvoiceItem        `json:"items,omitempty"`
+		Fee                          []xendit.InvoiceItem        `json:"fees,omitempty"`
+		ShouldAuthenticateCreditCard bool                        `json:"should_authenticate_credit_card,omitempty"`
+		RetailOutletName             string                      `json:"retail_outlet_name,omitempty"`
+		EwalletType                  string                      `json:"ewallet_type,omitempty"`
+		OnDemandLink                 string                      `json:"on_demand_link,omitempty"`
+		RecurringPaymentID           string                      `json:"recurring_payment_id,omitempty"`
 	}
 
 	err := app.readJSON(w, r, &callbackPayload)
