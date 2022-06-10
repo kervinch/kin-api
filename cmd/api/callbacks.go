f3056c0b8630aeb7d72f122f4a4f39d48320e44a
Kervin Christianata <kervin@ternakuang.id>
2022-06-10 14:34:58 +0700

    xendit callback

--- cmd/api/callbacks.go
+++ cmd/api/callbacks.go
@@ -10,9 +10,6 @@ import (
 // ====================================================================================
 
 func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	// headers := make(http.Header)
-	// headers.Set("x-callback-token", os.Getenv("XENDIT_CALLBACK_VERIFICATION_TOKEN"))
-
 	callbackToken := r.Header.Get("x-callback-token")
 
 	if callbackToken != os.Getenv("XENDIT_CALLBACK_VERIFICATION_TOKEN") {
@@ -20,47 +17,47 @@ func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	err := app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"callback_status": "OK"}, nil)
+	var callbackPayload struct {
+		ID                     string  `json:"id"`
+		ExternalID             string  `json:"external_id"`
+		UserID                 string  `json:"user_id"`
+		IsHigh                 bool    `json:"is_high"`
+		PaymentMethod          string  `json:"payment_method"`
+		Status                 string  `json:"status"`
+		MerchantName           string  `json:"merchant_name"`
+		Amount                 float64 `json:"amount"`
+		PaidAmount             float64 `json:"paid_amount"`
+		BankCode               string  `json:"bank_code"`
+		PaidAt                 string  `json:"paid_at"`
+		PayerEmail             string  `json:"payer_email"`
+		Description            string  `json:"description"`
+		AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
+		FeesPaidAmount         float64 `json:"fees_paid_amount"`
+		Updated                string  `json:"updated"`
+		Created                string  `json:"created"`
+		Currency               string  `json:"currency"`
+		PaymentChannel         string  `json:"payment_channel"`
+		PaymentDestination     string  `json:"payment_destination"`
+	}
+
+	err := app.readJSON(w, r, &callbackPayload)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"callback_status": "OK"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 
 	return
 
-	// var callbackPayload struct {
-	// 	ID                     string  `json:"id"`
-	// 	ExternalID             string  `json:"external_id"`
-	// 	UserID                 string  `json:"user_id"`
-	// 	IsHigh                 bool    `json:"is_high"`
-	// 	PaymentMethod          string  `json:"payment_method"`
-	// 	Status                 string  `json:"status"`
-	// 	MerchantName           string  `json:"merchant_name"`
-	// 	Amount                 float64 `json:"amount"`
-	// 	PaidAmount             float64 `json:"paid_amount"`
-	// 	BankCode               string  `json:"bank_code"`
-	// 	PaidAt                 string  `json:"paid_at"`
-	// 	PayerEmail             string  `json:"payer_email"`
-	// 	Description            string  `json:"description"`
-	// 	AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
-	// 	FeesPaidAmount         float64 `json:"fees_paid_amount"`
-	// 	Updated                string  `json:"updated"`
-	// 	Created                string  `json:"created"`
-	// 	Currency               string  `json:"currency"`
-	// 	PaymentChannel         string  `json:"payment_channel"`
-	// 	PaymentDestination     string  `json:"payment_destination"`
-	// }
-
-	// err := app.readJSON(w, r, &callbackPayload)
+	// orderID, err := strconv.ParseInt(callbackPayload.ExternalID, 10, 64)
 	// if err != nil {
 	// 	app.badRequestResponse(w, r, err)
 	// 	return
 	// }
-
-	// orderID, err := strconv.ParseInt(callbackPayload.ExternalID, 10, 64)
-	// // if err != nil {
-	// // 	app.badRequestResponse(w, r, err)
-	// // 	return
-	// // }
 
 	// err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"external_id": callbackPayload.ExternalID}, nil)
 	// if err != nil {
