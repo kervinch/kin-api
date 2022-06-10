acfa04e29eaf23d63da2c84d20b6c4aab5737b67
Kervin Christianata <kervin@ternakuang.id>
2022-06-10 14:28:13 +0700

    xendit callback

--- cmd/api/callbacks.go
+++ cmd/api/callbacks.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
-
-	"github.com/kervinch/internal/data"
 )
 
 // ====================================================================================
@@ -25,74 +20,81 @@ func (app *application) invoiceCallbackHandler(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	var callbackPayload struct {
-		ID                     string  `json:"id"`
-		ExternalID             string  `json:"external_id"`
-		UserID                 string  `json:"user_id"`
-		IsHigh                 bool    `json:"is_high"`
-		PaymentMethod          string  `json:"payment_method"`
-		Status                 string  `json:"status"`
-		MerchantName           string  `json:"merchant_name"`
-		Amount                 float64 `json:"amount"`
-		PaidAmount             float64 `json:"paid_amount"`
-		BankCode               string  `json:"bank_code"`
-		PaidAt                 string  `json:"paid_at"`
-		PayerEmail             string  `json:"payer_email"`
-		Description            string  `json:"description"`
-		AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
-		FeesPaidAmount         float64 `json:"fees_paid_amount"`
-		Updated                string  `json:"updated"`
-		Created                string  `json:"created"`
-		Currency               string  `json:"currency"`
-		PaymentChannel         string  `json:"payment_channel"`
-		PaymentDestination     string  `json:"payment_destination"`
-	}
-
-	err := app.readJSON(w, r, &callbackPayload)
-	if err != nil {
-		app.badRequestResponse(w, r, err)
-		return
-	}
-
-	orderID, err := strconv.ParseInt(callbackPayload.ExternalID, 10, 64)
-	// if err != nil {
-	// 	app.badRequestResponse(w, r, err)
-	// 	return
-	// }
-
-	err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"external_id": callbackPayload.ExternalID}, nil)
+	err := app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"callback_status": "OK"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 
 	return
 
-	order, err := app.gorm.Orders.Get(orderID)
-	if err != nil {
-		switch {
-		case errors.Is(err, data.ErrRecordNotFound):
-			app.notFoundResponse(w, r)
-		default:
-			app.serverErrorResponse(w, r, err)
-		}
-		return
-	}
+	// var callbackPayload struct {
+	// 	ID                     string  `json:"id"`
+	// 	ExternalID             string  `json:"external_id"`
+	// 	UserID                 string  `json:"user_id"`
+	// 	IsHigh                 bool    `json:"is_high"`
+	// 	PaymentMethod          string  `json:"payment_method"`
+	// 	Status                 string  `json:"status"`
+	// 	MerchantName           string  `json:"merchant_name"`
+	// 	Amount                 float64 `json:"amount"`
+	// 	PaidAmount             float64 `json:"paid_amount"`
+	// 	BankCode               string  `json:"bank_code"`
+	// 	PaidAt                 string  `json:"paid_at"`
+	// 	PayerEmail             string  `json:"payer_email"`
+	// 	Description            string  `json:"description"`
+	// 	AdjustedReceivedAmount float64 `json:"adjusted_received_amount"`
+	// 	FeesPaidAmount         float64 `json:"fees_paid_amount"`
+	// 	Updated                string  `json:"updated"`
+	// 	Created                string  `json:"created"`
+	// 	Currency               string  `json:"currency"`
+	// 	PaymentChannel         string  `json:"payment_channel"`
+	// 	PaymentDestination     string  `json:"payment_destination"`
+	// }
 
-	order.Status = strings.ToLower(callbackPayload.Status)
+	// err := app.readJSON(w, r, &callbackPayload)
+	// if err != nil {
+	// 	app.badRequestResponse(w, r, err)
+	// 	return
+	// }
 
-	err = app.gorm.Orders.Update(order)
-	if err != nil {
-		switch {
-		case errors.Is(err, data.ErrRecordNotFound):
-			app.notFoundResponse(w, r)
-		default:
-			app.serverErrorResponse(w, r, err)
-		}
-		return
-	}
+	// orderID, err := strconv.ParseInt(callbackPayload.ExternalID, 10, 64)
+	// // if err != nil {
+	// // 	app.badRequestResponse(w, r, err)
+	// // 	return
+	// // }
 
-	err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"order_id": orderID}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-	}
+	// err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"external_id": callbackPayload.ExternalID}, nil)
+	// if err != nil {
+	// 	app.serverErrorResponse(w, r, err)
+	// }
+
+	// return
+
+	// order, err := app.gorm.Orders.Get(orderID)
+	// if err != nil {
+	// 	switch {
+	// 	case errors.Is(err, data.ErrRecordNotFound):
+	// 		app.notFoundResponse(w, r)
+	// 	default:
+	// 		app.serverErrorResponse(w, r, err)
+	// 	}
+	// 	return
+	// }
+
+	// order.Status = strings.ToLower(callbackPayload.Status)
+
+	// err = app.gorm.Orders.Update(order)
+	// if err != nil {
+	// 	switch {
+	// 	case errors.Is(err, data.ErrRecordNotFound):
+	// 		app.notFoundResponse(w, r)
+	// 	default:
+	// 		app.serverErrorResponse(w, r, err)
+	// 	}
+	// 	return
+	// }
+
+	// err = app.writeJSON(w, http.StatusOK, http.StatusText(http.StatusOK), envelope{"order_id": orderID}, nil)
+	// if err != nil {
+	// 	app.serverErrorResponse(w, r, err)
+	// }
 }
